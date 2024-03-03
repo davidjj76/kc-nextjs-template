7d5ac66568bbc2fdde32dad8a953397ffb663bc7
David Jiménez <davidjj76@gmail.com>
2024-03-03 20:01:35 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,19 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    colors: {
+      base: colors.gray[800],
+      accent: colors.orange[600],
+      header: colors.slate[900],
+    },
+    borderColor: { DEFAULT: colors.gray[200] },
+  },
   plugins: [],
 };
 export default config;
